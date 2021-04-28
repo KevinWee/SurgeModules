8041d7c4698a3391c2ef06d2df74a30cb4482d87
Kevin Wee <kevinwee@163.com>
2021-04-28 21:48:00 +0800

    Update fanqie.js

--- Scripts/fanqie.js
+++ Scripts/fanqie.js
@@ -1,1 +1,14 @@
-$done({});
+const path1 = "fanqie.tv";
+const path2 = "wareBusiness";
+const url = $request.url;
+const body = $response.body;
+
+if (url.indexOf(path1) != -1) {
+    let obj = JSON.parse(body);
+    obj.data.playAd = 0;
+    obj.data.splashAd = 0;
+    obj.data.playSwitchAd = 0;
+    $done({ body: JSON.stringify(obj) });
+}else{
+    $done({});
+}
