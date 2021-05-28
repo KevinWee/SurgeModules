56884eacd05bf03cba3eca37ae41f08744a71110
Kevin Wee <kevinwee@163.com>
2021-05-28 14:27:00 +0800

    Create newsmth.js

---
+++ Scripts/newsmth.js
@@ -0,0 +1,14 @@
+const path1 = "newsmth.net";
+const url = $request.url;
+const body = $response.body;
+
+if (url.indexOf(path1) != -1) {
+    let obj = JSON.parse(body);
+    let articles = obj.data.articles;
+    for(j = 0,len=articles.length; j < len; j++) {
+      articles[j].body.replace(/<div class="article-quote">/, '<br/><div class="article-quote">');
+    }
+    $done({ body: JSON.stringify(obj) });
+}else{
+    $done({});
+}
