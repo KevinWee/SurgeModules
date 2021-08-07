101584ade9747adc269028786fcd1bbc0a6b36e1
Kevin Wee <kevinwee@163.com>
2021-08-07 14:12:12 +0800

    Update ftpark.js

--- Scripts/ftpark.js
+++ Scripts/ftpark.js
@@ -3,13 +3,13 @@ const url = $request.url;
 const body = $response.body;
 
 if (url.indexOf(path1) != -1) {
-	let items = JSON.parse(body);
+  let items = JSON.parse(body);
   let itemArray = items.data;
   let itemStr="";
   for(j = 0,len=itemArray.length; j < len; j++) {
     let item = itemArray[j];
     if(item.waitTime>0){
-      itemStr = itemStr + item.itemName+" "+item.distanceStr+" "+item.waitTime+" "+item.heightStr;
+      itemStr = itemStr + item.itemName+" "+item.distanceStr+" 需等候"+item.waitTime+"分钟 "+item.heightStr+"\r\n";
     }
   }
   if(itemStr!=""){
